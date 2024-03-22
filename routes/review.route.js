579096fba1de018cfa3ca5ad0e7a4c17e83dcd15
samaralielsayed <engsamar105@gmail.com>
2024-03-22 04:34:02 +0200

    edit in Reviews edit Route

--- routes/review.route.js
+++ routes/review.route.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const controllers = require("../controllers/reviews.controller");
 const { auth } = require("../middleware/auth");
 
-router.get("/:id/reviews", controllers.getProductReviews);
+//router.get("/:id/reviews", controllers.getProductReviews);
 // router.post("/:id/ratings",auth, controllers.addProductRating);
 // router.post("/:id/reviews",auth, controllers.addProductReviews);
+router.get("/:id/reviews", controllers.getProductReviews);
 router.post("/:id/ratings",controllers.addProductRating);
 router.post("/:id/reviews", controllers.addProductReviews);
 router.delete("/:id/reviews", controllers.deleteProductReviews);
-router.patch("/:id/reviews", controllers.editProductReviews);
+router.patch("/:id/reviews",controllers.editProductReviews);
 
 
 module.exports = router;
